6d789c6bc7e7355495def9dd11ac3e0f11421f0b
LuisValrod <luisrodriguezvalido@gmail.com>
2024-01-25 16:22:21 +0000

    improved coded with slicing

--- main.py
+++ main.py
@@ -54,10 +54,8 @@ while game_is_on:
     # Detect collision with tail
     # if head collides with any segment in the tail:
     # trigger game_over
-    for segment in snake.segments:
-        if segment == snake.head:
-            pass
-        elif snake.head.distance(segment) < 10:
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
             game_is_on = False
             scoreboard.game_over()
 
