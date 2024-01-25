2f7fd9ed1f82ca6e76ec31efce3198e234a18622
LuisValrod <luisrodriguezvalido@gmail.com>
2024-01-25 14:49:30 +0000

    Food added, collision functionality with food added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from turtle import Screen, Turtle
+from turtle import Screen
 from snake import Snake
+from food import Food
 import time
 
 
@@ -10,16 +11,17 @@ screen.bgcolor('black')
 screen.title('My Snake Game - LRV')
 screen.tracer(0)
 snake = Snake()
+food = Food()
 
 difficulty_level = 0.1
-difficulty_prompt = screen.textinput(title='Difficulty Levels:', prompt="What level you prefer?\n - Easy "
-                                                                        "\n - Middle ""\n -Hard")
-if difficulty_prompt.lower() == 'easy':
-    difficulty_level = 0.3
-if difficulty_prompt.lower() == 'middle':
-    difficulty_level = 0.1
-if difficulty_prompt.lower() == 'hard':
-    difficulty_level = 0.05
+# difficulty_prompt = screen.textinput(title='Difficulty Levels:', prompt="What level you prefer?\n - Easy "
+#                                                                         "\n - Middle ""\n -Hard")
+# if difficulty_prompt and difficulty_prompt.lower() == 'easy':
+#     difficulty_level = 0.3
+# if difficulty_prompt and difficulty_prompt.lower() == 'middle':
+#     difficulty_level = 0.1
+# if difficulty_prompt and difficulty_prompt.lower() == 'hard':
+#     difficulty_level = 0.05
 
 screen.listen()
 
@@ -28,13 +30,16 @@ screen.onkey(snake.right, "Right")
 screen.onkey(snake.up, "Up")
 screen.onkey(snake.down, "Down")
 
-
-
 game_is_on = True
 
 while game_is_on:
     screen.update()
     time.sleep(difficulty_level)
     snake.move()
+
+    # Detect collision with food.
+    if snake.head.distance(food) < 15:
+        food.refresh()
+
 
 screen.exitonclick()
